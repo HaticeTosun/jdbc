855177fb5e21f1a9649b44ed3fd3c765d03ee838
HaticeTosun <56235635+HaticeTosun@users.noreply.github.com>
2020-01-25 12:08:41 -0500

    connect disconnect

--- src/test/java/TestCases.java
+++ src/test/java/TestCases.java
@@ -1,3 +1,4 @@
+
 import org.testng.annotations.Test;
 
 import java.sql.Connection;
@@ -7,9 +8,9 @@ import java.sql.SQLException;
 public class TestCases {
     @Test
     public void  test() throws SQLException {
-        String url = "jdbc:mysql://database-techno.c771qxmldhez.us-east-2.rds.amazonaws.com:3306/daulet2030_studens_database";
-        String user = "daulet2030";
-        String password = "daulet2030@gmail.com";
+        String url = "jdbc:mysql://database-techno.c771qxmldhez.us-east-2.rds.amazonaws.com:3306/haticetosuntr_studens_database";
+        String user = "haticetosuntr";
+        String password = "haticetosuntr@gmail.com";
         Connection connection = DriverManager.getConnection(url, user, password);
 
     }
